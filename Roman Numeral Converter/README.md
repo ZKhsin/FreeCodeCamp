b2cf510aaf9bd35286f3e657dea2413bed6630d6
hsin86 <kxin666@gmail.com>
2020-06-12 19:25:19 +0800

    add solution

---
+++ Roman Numeral Converter/README.md
@@ -0,0 +1,3 @@
+# Roman Numeral Converter
+
+## Solution
---
+++ Roman Numeral Converter/script.js
@@ -0,0 +1,27 @@
+function convertToRoman(num) {
+  const convert = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let roman = "";
+  for (let i in convert) {
+    while (num >= convert[i]) {
+      roman += i;
+      num -= convert[i];
+    }
+  }
+  return roman;
+}
+
+convertToRoman(36);
