43b07980388fedc6822c178b8c037471a5a85948
hsin86 <kxin666@gmail.com>
2020-06-18 20:08:09 +0800

    add project

---
+++ Cash Register/script.js
@@ -0,0 +1,70 @@
+const denomination = [
+  { name: "ONE HUNDRED", value: 100.0 },
+  { name: "TWENTY", value: 20.0 },
+  { name: "TEN", value: 10.0 },
+  { name: "FIVE", value: 5.0 },
+  { name: "ONE", value: 1.0 },
+  { name: "QUARTER", value: 0.25 },
+  { name: "DIME", value: 0.1 },
+  { name: "NICKEL", value: 0.05 },
+  { name: "PENNY", value: 0.01 },
+];
+
+function checkCashRegister(price, cash, cid) {
+  let output = { status: null, change: [] };
+  let change = cash - price;
+
+  let register = cid.reduce(
+    function (acc, curr) {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
+
+  // Handle exact change
+  if (register.total === change) {
+    output.status = "CLOSED";
+    output.change = cid;
+    return output;
+  }
+
+  // Handle obvious insufficient funds
+  if (register.total < change) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  // Loop through the denomination array
+  var change_arr = denom.reduce(function (acc, curr) {
+    var value = 0;
+    // While there is still money of this type in the drawer
+    // And while the denomination is larger than the change remaining
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+
+      // Round change to the nearest hundreth deals with precision errors
+      change = Math.round(change * 100) / 100;
+    }
+    // Add this denomination to the output only if any was used.
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc; // Return the current change_arr
+  }, []); // Initial value of empty array for reduce
+
+  // If there are no elements in change_arr or we have leftover change, return
+  // the string "Insufficient Funds"
+  if (change_arr.length < 1 || change > 0) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+
+  // Here is your change.
+  output.status = "OPEN";
+  output.change = change_arr;
+  return output;
+}
